b7dd1bb1c64ef0f776cbd45e21f5ca3c991473f3
Mr. Python <mrpython@python.local>
2017-04-26 20:45:39 +0430

    fix user authenticate issue

--- panel/private/controllers/user.php
+++ panel/private/controllers/user.php
@@ -13,12 +13,12 @@
 		{
 			$user = $this -> login -> authenticate($_params['username'], $_params['password']);
 
-			if ($user != null) $result = self::authorize($user);
+			if ($user != null) $result = $this -> authorize($user);
 
-			return ['token' => $token];
+			return ['token' => $result];
 		}
 
-		public static function authorize ($_user)
+		public function authorize ($_user)
 		{
 			if ($_user['token'] != null && Token::verify($_user['id'], $_user['token'], TOKEN_SECRET)) $token = $_user['token'];
 			else $token = Token::generate($_user['id'], TOKEN_SECRET, LOGIN_TIMEOUT);
