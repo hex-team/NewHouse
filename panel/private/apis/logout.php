c0865615d6e9624d5321a56045820e8d2b2a4cc7
Mr. Python <mrpython@python.local>
2017-05-01 14:05:25 +0430

    fix logout issue

--- panel/private/apis/logout.php
+++ panel/private/apis/logout.php
@@ -8,15 +8,20 @@
 
 	$header[$allowedParams[0]] = substr($header[$allowedParams[0]], 0, MAX_TOKEN_LENGTH);
 
-	$authentication = Token::verify($header['token']);
+	$authorization = Token::verify($header['token']);
 
-	if (!$authentication) Maintenance::handleApiActions(REQUEST_MSG, $authentication);
+	if (!$authorization)
+	{
+		http_response_code(UNAUTHORIZED);
+
+		Maintenance::handleApiActions(REQUEST_MSG, $authorization);
+	}
 
 	$obj = new User();
 
 	$result = $obj -> logout($header);
 
-	if (!$result) http_response_code(UNAUTHORIZED);
+	if (!$result) http_response_code(Bad_REQUEST);
 	else http_response_code(OK);
 
 	Maintenance::handleApiActions((empty($result) ? REQUEST_MSG : REQUEST_SUCCESSFULL_MSG), $result);
