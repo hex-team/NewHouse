66c3a75be91d4fcee0a372090d6e295f47daf531
Mr. Python <mrpython@python.local>
2017-05-27 12:41:51 +0430

    edit htaccess for remove widget

---
+++ panel/private/apis/addwidget.php
@@ -0,0 +1,17 @@
+<?php
+	if (!User::authorize()) die();
+
+	$allowedParams = explode(DELIMITER, ADDWIDGET_PARAMS);
+
+	$params = Validation::post($allowedParams, false);
+
+	if (!$params) die();
+
+	$params[$allowedParams[0]] = substr($params[$allowedParams[0]], 0, MAX_NAME_LENGTH);
+
+	$widget = new Widget();
+
+	if ($widget -> add($params)) http_response_code(OK);
+
+	die();
+?>
